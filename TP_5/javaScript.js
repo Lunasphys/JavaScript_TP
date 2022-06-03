3ee96558f56d50040b2c13a25441557dda1e0358
unknown <laurie.jeham@hotmail.fr>
2022-06-03 16:18:57 +0200

    trois colonne items aléatoire ok

--- TP_5/javaScript.js
+++ TP_5/javaScript.js
@@ -1,14 +1,17 @@
 console.log("salut, salut");
 
 document.querySelector('button')
-.addEventListener('click', function() {
+.addEventListener('click', ()=> {
     const items = [
         'casino',
         'cherry',
         'diamond',
         'lemon',
     ]
-    const random = Math.floor(Math.random() * items.length);
-    const selectedItem = items[random];
-    document.querySelector('img').src = "img/items/" + selectedItem + ".png";
+
+    document.querySelectorAll('img').forEach((imgEl)=>{
+        const random = Math.floor(Math.random() * items.length);
+        const selectedItem = items[random];
+        imgEl.src=`img/items/${selectedItem}.png`;
+    });
 });
